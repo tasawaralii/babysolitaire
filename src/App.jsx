7ba35ab891c4962da6e5b22f1cc1977d893e423d
Baby x Boss <speedodubbers@gmail.com>
2025-10-23 21:31:37 +0500

    React Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-full text-amber-300 h-full bg-black'>BabyxBoss Solitaire</div>
+    </>
+  )
+}
+
+export default App
